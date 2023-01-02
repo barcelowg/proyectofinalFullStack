a58f9e4791a78776523559c1d80a4ea9667a5d7c
GermanB <wbarcelo@gmail.com>
2023-01-02 20:12:23 -0300

    se acrualizaron los archivos inicio.inc.php y cargarDB.php

---
+++ cargarDB.php
@@ -0,0 +1,48 @@
+
+<?php
+// Datos para la conexion a la base de datos
+$servername = "localhost";
+$username = "c2081732_climasl";
+$password = "fumabuDA00";
+$dbname = "c2081732_climasl";
+
+// Crea conexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Chequea conexion
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+// Cargar los registros
+$stmt = $conn->prepare("INSERT INTO resumendia (Fecha, TempMax, TempMin, HumMax, HumMin, PresMax, PresMin, DirVientoMax, DirVientoMin, VientoMax, VientoMin, DirVieProm, Rafagas, LluvHoy, LluvAyer, LluvMes, LluvA)  
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("sssssssssssssssss",$Fecha, $TempMax, $TempMin, $HumMax, $HumMin, $PresMax, $PresMin, $DirVientoMax, $DirVientoMin, $VientoMax, $VientoMin, $DirVieProm, $Rafagas, $LluvHoy, $LluvAyer, $LluvMes, $LluvA);
+
+// Seteo de parametros Meteobridge a cargar
+$Fecha = "2023-01-01";
+$TempMax = "23.5";
+$TempMin = "14.0";
+$HumMax = "99";
+$HumMin = "37";
+$PresMax = "994.30";
+$PresMin = "989.90";
+$DirVientoMax = "NW";
+$DirVientoMin = "N";
+$VientoMax = "19.4";
+$VientoMin = "0.0";
+$DirVieProm = "WSW";
+$Rafagas = "39.2";
+$LluvHoy = "11.00";
+$LluvAyer = "0.0";
+$LluvMes = "11.0";
+$LluvA = "11.0";
+
+$stmt->execute();
+
+echo "Registro cargado correctamente";
+
+$stmt->close();
+$conn->close();
+?>
--- inc/inicio.inc.php
+++ inc/inicio.inc.php
@@ -1,114 +1,108 @@
 <!-- Título -->
     <div class="container mt-3 ">
-    <div class="mt-2 p-2 bg-transparent text-dark ">
-        <h3>Inicio</h3> 
-        <span>En esta sección se muestran valores registrados en la localidad de <a target="_blank" href="https://www.google.com.ar/maps/place/Estaci%C3%B3n+Meteorol%C3%B3gica+de+IAMOFM/@-33.8805725,-59.8807454,16z/data=!4m5!3m4!1s0x0:0x6478ee5e22348eaf!8m2!3d-33.880572!4d-59.876368">Santa Lucia,</a> provincia de Buenos Aires; proporcionados por una Estación Meteorológica Automática WH-1081.</span>
-    </div>
+        <div class="mt-2 p-2 bg-transparent text-dark ">
+            <h3>Inicio</h3> 
+            <span>En esta sección se muestran valores registrados en la localidad de <a target="_blank" href="https://www.google.com.ar/maps/place/Estaci%C3%B3n+Meteorol%C3%B3gica+de+IAMOFM/@-33.8805725,-59.8807454,16z/data=!4m5!3m4!1s0x0:0x6478ee5e22348eaf!8m2!3d-33.880572!4d-59.876368">Santa Lucia,</a> provincia de Buenos Aires; proporcionados por una Estación Meteorológica Automática WH-1081.</span>
+        </div>
 
     <br>
 
 <!-- Contenido -->
-    <!-- Tarjeta temperatura -->
+    <!-- Tarjetas 1-->
     <div class="container-fluid mt-4">
         <div class="row">
+            <!-- Temperatura -->
             <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
-                <p><i class="fas fa-temperature-high"></i>  Temperatura 12.3&nbsp;°C</p>
+                <p><i class="fas fa-temperature-high"></i>  Temperatura 28.9°C</p>
                 <ul>
-                    <li>Sensación térmica: 12.3&nbsp;°C</li>
-                    <li>Punto de rocío: 7.8&nbsp;°C</li>
-                    <li>Máxima temperatura: 12.5&nbsp;°C</li>
-                    <li>Mínima temperatura:7.4&nbsp;°C</li>
+                    <li>Sensación térmica: 28.9°C</li>
+                    <li>Punto de rocío: 13.2°C</li>
+                    <li>Máxima temperatura: 31.2°C</li>
+                    <li>Mínima temperatura:14.9°C</li>
                 </ul>
             </div>
-
+            <!-- Humedad -->
             <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
-                <p><i class="fas fa-tint"></i>  Humedad <br>74.0&nbsp;%</p>
+                <p><i class="fas fa-tint"></i>  Humedad <br>38.0%</p>
                     <ul>
-                        <li>Máxima humedad: 97.0&nbsp;%</li>
-                        <li>Mínima humedad: 92.0&nbsp;%</li>
-                        <li>Base de nubes: 73 m</li>
+                        <li>Máxima humedad: 96.0%</li>
+                        <li>Mínima humedad: 28.0%</li>
+                        <li>Base de nubes: 1914 m</li>
                     </ul>
             </div>
+            <!-- Viento -->
             <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
-                <p><i class="fas fa-wind"></i> Viento<br>2.5&nbsp;km/h</p>
+                <p><i class="fas fa-wind"></i> Viento<br>1.1km/h</p>
                     <ul>
-                        <li>Velocidad (ráfaga): 7.2 km/h</li>
+                        <li>Velocidad (ráfaga): 2.5 km/h</li>
                         <li>Dirección: S </li> 
-                        </ul>
-            </div>
-            </div>
-        </div>
-
-
-<div class="container-fluid mt-4">
-        <div class="row">
-            <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
-                <p><i class="fas fa-cloud-showers-heavy"></i> Lluvia<br> 0.0&nbsp;mm</p>
-                <ul>
-                    <li>Lluvia de hoy: 0.0&nbsp;mm</li>
-                    <li>Lluvia del mes: 0.0&nbsp;mm</li>
-                    <li>Lluvia de esta hora:0.0&nbsp;mm</li>
-                    <li>Ritmo de lluvia: 0.0&nbsp;mm/hora</li>
-                    <li>Lluvia del año: 420.6&nbsp;mm</li>
-                        
-                </ul>
-            </div>
-
-            <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
-                <p><i class="fas fa-tachometer-alt"></i> Presión<br> 1000.5&nbsp;hPa</p>
-                <ul>
-                    <li>Máxima Presión: 1000.7&nbsp;hPa</li>
-                    <li>Mínima Presión: 998.4&nbsp;hPa</li>
-                </ul>
-            </div>
-
-            <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
-                <p><i class="fas fa-cloud-sun-rain"></i> AYER<br></p>
-                <ul>
-                    <li>Máxima temperatura: 29.0&nbsp;°C</li>
-                    <li>Mínima temperatura:13.9&nbsp;°C</li>
-                    <li>Máxima Presión: 1000.5&nbsp;hPa</li>
-                    <li>Mínima Presión: 997.3&nbsp;hPa</li>
-                    <li>Lluvia total: 0.0&nbsp;mm</li>
-                    <li>Velocidad del viento: 9.7 km/h</li>
-                    <li>Ráfagas de viento: 18.4&nbsp;km/h</li>
-                        
                     </ul>
             </div>
-
+            </div>
         </div>
-</div>
-<hr>
-<div class="container-fluid mt-3">
-  
-  <div class="row">
-    <div class="col-sm-2 p-3 bg-transparent"><strong>Salida del sol:</strong> 07:16</div>
-    <div class="col-sm-2 p-3 bg-transparent"><strong>Puesta del sol:</strong> 18:42</div>
-    <div class="col-sm-2 p-3 bg-transparent"><strong>Puesta de la luna:</strong> 14:16</div>
-    <div class="col-sm-2 p-3 bg-transparent"><strong>Duración del día:</strong> 11:27</div>
-    <div class="col-sm-2 p-3 bg-transparent"><strong>Salida de la luna:</strong> 04:27</div>
-  </div>
-</div>
 
+    <!-- Tarjeras 2 -->
+    <div class="container-fluid mt-4">
+            <div class="row">
+                <!-- Lluvia -->
+                <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
+                    <p><i class="fas fa-cloud-showers-heavy"></i> Lluvia<br> 0.3mm</p>
+                    <ul>
+                        <li>Lluvia de hoy: 0.3mm</li>
+                        <li>Lluvia del mes: 11.3mm</li>
+                        <li>Lluvia de esta hora:0.0mm</li>
+                        <li>Ritmo de lluvia: 0.0mm/hora</li>
+                        <li>Lluvia del año: 11.3mm</li>
+                            
+                    </ul>
+                </div>
+                <!-- Presión -->
+                <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
+                    <p><i class="fas fa-tachometer-alt"></i> Presión<br> 989.7hPa</p>
+                    <ul>
+                        <li>Máxima Presión: 996.1hPa</li>
+                        <li>Mínima Presión: 989.2hPa</li>
+                    </ul>
+                </div>
+                <!-- Ayer -->
+                <div class="col-sm-4 col-lg-3 p-3 bg-secondary bg-opacity-10 text-dark">
+                    <p><i class="fas fa-cloud-sun-rain"></i> AYER<br></p>
+                    <ul>
+                        <li>Máxima temperatura: 23.5°C</li>
+                        <li>Mínima temperatura:14.0°C</li>
+                        <li>Máxima Presión: 994.3hPa</li>
+                        <li>Mínima Presión: 989.9hPa</li>
+                        <li>Lluvia total: 11.0mm</li>
+                        <li>Velocidad del viento: 19.4 km/h</li>
+                        <li>Ráfagas de viento: 39.2km/h</li>
+                            
+                        </ul>
+                </div>
 
-
+            </div>
+    </div>
+    
     <hr>
 
+    <!-- Datos del día -->
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col-sm-2 p-1 bg-transparent"><strong>Salida del sol:</strong> 05:53</div>
+            <div class="col-sm-2 p-1 bg-transparent"><strong>Puesta del sol:</strong> 20:14</div>
+            <div class="col-sm-2 p-1 bg-transparent"><strong>Duración del día:</strong> 14:21</div>
+            <div class="col-sm-2 p-1 bg-transparent"><strong>Puesta de la luna:</strong> 16:45</div>
+            <div class="col-sm-2 p-1 bg-transparent"><strong>Salida de la luna:</strong> 16:45</div>
+        </div>
+    </div>
+
+    <hr>
+    
+    <!-- Datos de actualización -->
     <div class="container-fluid mt-2">
-  
-  <div class="row">
-    <div class="col-sm-5 p-1 bg-transparent"><span>Actualizado el 08-04-2022   08:42 por Software: <a href="http://www.meteobridge.com" target="_blank">MeteoBridge</a> ;vv4.0 (13084)</span></div>
-    <div class="col-sm-5 p-1 bg-transparent"><span>Latitud -33.880844 Longitud -59.875634 Altitud 6.0</span></div>
+        <div class="row">
+            <div class="col-sm-5 p-1 bg-transparent"><span>Actualizado el <strong>02-01-2023   20:04</strong> por Software:<a href="http://www.meteobridge.com" target="_blank">MeteoBridge</a> vv4.0</span></div>
+            <div class="col-sm-5 p-1 bg-transparent"><span>Latitud -33.880844 &bull; Longitud -59.875634 &bull; Altitud 51.0</span></div>
+        </div>
+    </div>
     
-  </div>
-</div>
-<hr>
-    
-
-
-
-
-
-
-    <!-- JavaScript Bundle with Popper -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> -->
+    <hr>
